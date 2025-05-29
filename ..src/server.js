d6d3350d095e05f2c654abdce380e77925a722f6
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2025-05-29 17:48:04 +0530

    nodejs

--- ..src/server.js
+++ ..src/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
+const Path =require('path');
+
 const port = 4040;
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', Path.join(__dirname,'./src/views'));
 
 app.get('/',(req,res)=>{
-    return res.render('/index');
+    return res.render('index');
 });
 
 app.listen(port,(err)=>{
